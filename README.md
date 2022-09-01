013a3632417f338375c32bdc16dacdd0848583ec
PeterJackNaylor <peter.jack.naylor@gmail.com>
2022-09-01 18:32:23 +0900

    fixing moco_exfixing moco_expp

--- README.md
+++ README.md
@@ -23,23 +23,36 @@ make setup_conda
 # download the data
 make download_data
 
-# run the experiment
+# run the main experiment
 make experiment
+
+# run the supplementary experiment of MoCo
+make moco_experiment
+
+# run the supplementary experiment to see the effect of different augmentations on the test set
+make augmentation_experiment:
 ```
 
 
 ## Hyper parameter-settings
-We used the following hyper-parameters in the paper:
+To reproduce the main experiment in the paper please use the following hyper-parameters:
 ``` python
 methods_selection = ["ascending", "descending"]
 LAMBDA = [0.00078125, 0.0078125, 0.078125]
 LR = [0.0001, 0.001, 0.01, 0.1]
 WD = [0, 1e-6, 1e-4, 1e-2, 1.]
-FEATURE_DIM = [32, 64, 128] 
+FEATURE_DIM = [32, 64, 128]
+MB = [32768, 65536]
+KS = [3, 5]
+epochs = [ "tnbc": 100, "consep": 100, "pannuke": 50, "tnbcpadded": 100, "conseppadded": 100, "pannukepaddded": 50]
+bs = [ "tnbc": [128], "consep": [128], "pannuke": [512], "tnbcpadded": [64], "conseppadded": [64], "pannukepadded": [64]]
+number_bs = [0] // This parameter is to iterate over the batch size, please modify if you specify many batch size
+
 models = ["ModelSDRN", "ModelSRN"]
 opt = ["--inject_size", "--no_size"]
 repetition = 20
 ```
+
 
 ## Cite
 Please cite our paper if you use our dataset or code.
--- src/python/moco_plot.py
+++ src/python/moco_plot.py
@@ -10,8 +10,7 @@ def main():
     tmp = tmp.reset_index()
     tmp["bs"] = tmp.name.apply(lambda x: x.split("_")[-2])
     tmp["MB"] = tmp.name.apply(lambda x: x.split("_")[-3])
-    for var in ["test_score", "knnscoreK=40"]:
-        import pdb; pdb.set_trace()
+    for var in ["test_score"]:
         tmp[var + "_std"] = np.array([
             f"{tmp[var].values[i] * 100:.1f}"
             + " \pm "
@@ -26,7 +25,6 @@ def main():
             "lr",
             "wd",
             "test_score_std",
-            "knnscoreK=40_std",
         ]
     ]
     tmp.columns = [
@@ -36,36 +34,12 @@ def main():
         "LR",
         "WD",
         "Linear ({})".format(data),
-        "knn ({})".format(data),
     ]
     tmp = tmp.drop("name", axis=1)
     tmp["BS"] = tmp["BS"].astype(int)
     tmp["MB"] = tmp["MB"].astype(int)
     tmp = tmp.sort_values(["MB", "BS", "LR", "WD"])
-    import pdb; pdb.set_trace()
-    tmp = tmp.set_index(["MB", "BS", "LR", "WD"])
-    # tmp = tmp.loc[tmp.groupby(["bs", "MB"])["train_score"].idxmax()]
-    # # bs = list(tmp.bs.unique())
-    # # mb = list(tmp.MB.unique())
-    # # for bs_ in bs:
-    # #     for mb_ in mb:
-    # #         tmpi = tmp.loc[(tmp.bs == bs_) & (tmp.MB == mb_)]
-            
-    # #         y = []
-    # #         for k in ks:
-    # #             tmpi[]
-    # ks = [40]
-    # df = pd.DataFrame()
-    # df_index = 0
-    # for i in tmp.index:
-    #     for k in ks:
-    #         df.loc[df_index, 'tag'] = f"({tmp.loc[i, 'bs']}, {tmp.loc[i, 'MB']})"
-    #         df.loc[df_index, 'k'] = k
-    #         df.loc[df_index, 'score'] = tmp.loc[i, f'knnscoreK={k}']
-    #         df_index += 1
-    
-    # fig = px.line(df, x="k", y="score", color='tag')
-    # fig.write_image("scores.png")
+    tmp.to_csv("moco_experiments.csv")
 
 
 
